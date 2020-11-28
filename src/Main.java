62f1c921823c4dc69a6dc83abdfb072cd78579db
Simpelax <ggguuuggg43@gmail.com>
2020-11-29 00:19:39 +0300

    111

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.io.BufferedReader;
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+        int cart = 27;
+        int crot = cart;
+        if (cart == crot){
+            System.out.println("fine");
+        }
+       exp t= new exp();
+        t.toString();
     }
 }
---
+++ src/exp.java
@@ -0,0 +1,10 @@
+import java.io.*;
+public class exp {
+    public static String getString() throws IOException
+    {
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        String s = br.readLine();
+        return s;
+    }
+}
